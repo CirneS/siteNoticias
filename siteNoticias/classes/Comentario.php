b608af2f24862ef0bd764f58649714c5af2bfa86
CirneS <69870121+CirneS@users.noreply.github.com>
2021-01-08 10:31:34 -0300

    Update Comentario.php

--- siteNoticias/classes/Comentario.php
+++ siteNoticias/classes/Comentario.php
@@ -1,14 +1,5 @@
 <?php
-/**
- * Classe Comentario
- * Desenvolvida em aulas no Curso Técnico em Informática do CIMOL
- * @author Artur arturkontzm@gmail.com
- * @author Cândido candido.cimol@gmail.com
- * @version 0.1
- * @access public
- * @copyright GPL 2020, Inf63
- * @since 09/07/2020
-*/
+
 class Comentario{
     /**
      * @access private
@@ -122,10 +113,6 @@ class Comentario{
 
     /**
      * Método responsável por criar um novo comentário
-     * @access public
-     * @author Artur
-     * @since 12/08/2020
-     * @version 0.1
     */
     public function criar($noticia){
         $comentario = filter_input(INPUT_POST, "comment", FILTER_SANITIZE_STRING);
@@ -148,10 +135,7 @@ class Comentario{
 
     /**
      * Método responsável por listar os comentários
-     * @access public
-     * @author Artur
-     * @since 19/08/2020
-     * @version 0.1
+
     */
     public function listar($noticia){
         $mysql=Conexao::getConexao();
@@ -170,10 +154,7 @@ class Comentario{
     
     /**
      * Método responsável por listar os comentários
-     * @access public
-     * @author Artur
-     * @since 21/08/2020
-     * @version 0.2
+
     */
     public  function apagar($id, $noticia, $nome){
         if(isset($_SESSION['email'])){
